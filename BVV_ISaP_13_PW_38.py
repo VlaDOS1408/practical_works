a6e2a2ec77f36bb6ab264393f5f2b7206e08f271
VlaDOS1408 <vlad.buklovsky@gmail.com>
2024-06-27 18:39:00 +0400

    Add PW38.py

---
+++ BVV_ISaP_13_PW_38.py
@@ -0,0 +1,9 @@
+input_text = input("Enter string on english language: ")
+count_t = 0
+
+for char in input_text:
+    if char == "t":
+        count_t += 1
+print(f"The number of letters \"t\" in the line \"{input_text}\" is - {count_t}\nWith use cycle 'for'.")
+
+print(f"The number of letters \"t\" in the line \"{input_text}\" is - {input_text.lower().count("t")}\nWith use class 'string' function.")
